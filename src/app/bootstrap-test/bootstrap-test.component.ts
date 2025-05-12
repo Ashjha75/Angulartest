644a02613299174ca501c44cb19d1be27f353c8c
Ashish jha <ajha5645@gmail.com>
2025-05-12 11:47:17 +0530

    Enhance Bootstrap integration with navbar, alert system, and modal in Angular app

--- src/app/bootstrap-test/bootstrap-test.component.ts
+++ src/app/bootstrap-test/bootstrap-test.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bootstrap-test',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './bootstrap-test.component.html',
   styleUrl: './bootstrap-test.component.css'
 })
 export class BootstrapTestComponent {
-
+  alertMessage = 'Bootstrap has been successfully integrated into your Angular 17 project!';
+  alertType = 'success';
+  
+  changeAlert(type: string) {
+    this.alertType = type;
+    switch(type) {
+      case 'success':
+        this.alertMessage = 'Success! Bootstrap is working correctly.';
+        break;
+      case 'warning':
+        this.alertMessage = 'Warning! This is a test alert.';
+        break;
+      case 'danger':
+        this.alertMessage = 'Danger! This is a test alert.';
+        break;
+      case 'info':
+        this.alertMessage = 'Info! This is a test alert.';
+        break;
+      default:
+        this.alertMessage = 'Bootstrap has been successfully integrated into your Angular 17 project!';
+    }
+  }
 }
