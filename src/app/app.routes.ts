85215b64bc210fbbc695bf8df0d6d4575e5cb5d7
Ashish jha <ajha5645@gmail.com>
2025-05-12 11:40:19 +0530

    Remove default Angular styles and set up routing for Bootstrap test component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { BootstrapTestComponent } from './bootstrap-test/bootstrap-test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: BootstrapTestComponent },
+  { path: 'bootstrap-test', component: BootstrapTestComponent }
+];
