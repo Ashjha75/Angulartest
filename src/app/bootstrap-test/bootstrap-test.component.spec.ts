d3de74bd595a479a6d325304adb2700ab607f932
Ashish jha <ajha5645@gmail.com>
2025-05-12 11:39:15 +0530

    Integrate Bootstrap into Angular project with component setup and styles

---
+++ src/app/bootstrap-test/bootstrap-test.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BootstrapTestComponent } from './bootstrap-test.component';
+
+describe('BootstrapTestComponent', () => {
+  let component: BootstrapTestComponent;
+  let fixture: ComponentFixture<BootstrapTestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BootstrapTestComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(BootstrapTestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/bootstrap-test/bootstrap-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bootstrap-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './bootstrap-test.component.html',
+  styleUrl: './bootstrap-test.component.css'
+})
+export class BootstrapTestComponent {
+
+}
